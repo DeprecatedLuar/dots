734b4b74dadc409bf087d113a3c58f1cf3d6c7f7
DeprecatedLuar <rauassis2@gmail.com>
2026-06-07 15:49:30 -0300

    yolo commit

--- config/nixos/.system/services/available/nginx.nix
+++ config/nixos/.system/services/available/nginx.nix
@@ -4,10 +4,18 @@
   services.nginx = {
     enable = true;
 
-    # Allow traditional config file management
-    # Site configs can be managed in /etc/nginx/sites-available/
-    # and symlinked to /etc/nginx/sites-enabled/
+    # Allow traditional config file management via sites-enabled
+    appendHttpConfig = ''
+      include /etc/nginx/sites-enabled/*;
+    '';
   };
+
+  # Create sites-available and sites-enabled directories
+  systemd.tmpfiles.rules = [
+    "d /etc/nginx 0755 root root -"
+    "d /etc/nginx/sites-available 0755 root root -"
+    "d /etc/nginx/sites-enabled 0755 root root -"
+  ];
 
   # Open HTTP and HTTPS ports
   networking.firewall.allowedTCPPorts = [ 80 443 ];
