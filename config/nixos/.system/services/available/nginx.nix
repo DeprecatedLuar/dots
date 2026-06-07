3c856fbda3ae98a05e5caa334f34bf4698d0ac9a
DeprecatedLuar <rauassis2@gmail.com>
2026-06-07 16:06:31 -0300

    dots push

--- config/nixos/.system/services/available/nginx.nix
+++ config/nixos/.system/services/available/nginx.nix
@@ -4,21 +4,61 @@
   services.nginx = {
     enable = true;
 
-    # Allow traditional config file management via sites-enabled
-    appendHttpConfig = ''
-      include /etc/nginx/sites-enabled/*;
-    '';
+    # Recommended settings
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    # Virtual hosts
+    virtualHosts = {
+      "n8n.theparaloid.com" = {
+        enableACME = true;
+        forceSSL = true;
+
+        extraConfig = ''
+          client_max_body_size 100M;
+        '';
+
+        locations."/" = {
+          proxyPass = "http://localhost:5678";
+          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+
+            proxy_read_timeout 80s;
+            proxy_connect_timeout 80s;
+            proxy_send_timeout 80s;
+
+            chunked_transfer_encoding off;
+            proxy_buffering off;
+            proxy_cache off;
+          '';
+        };
+      };
+
+      "api.theparaloid.com" = {
+        enableACME = true;
+        forceSSL = true;
+
+        extraConfig = ''
+          client_max_body_size 100M;
+        '';
+
+        locations."/" = {
+          return = "404";
+        };
+      };
+    };
   };
 
-  # Allow nginx to bind to privileged ports
-  systemd.services.nginx.serviceConfig.AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
-
-  # Create sites-available and sites-enabled directories
-  systemd.tmpfiles.rules = [
-    "d /etc/nginx 0755 root root -"
-    "d /etc/nginx/sites-available 0755 root root -"
-    "d /etc/nginx/sites-enabled 0755 root root -"
-  ];
+  # ACME configuration for Let's Encrypt
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "rlas00sil@gmail.com";
+  };
 
   # Open HTTP and HTTPS ports
   networking.firewall.allowedTCPPorts = [ 80 443 ];
